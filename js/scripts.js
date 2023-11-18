60448e3a57523fb2931238525d325f5c9b85a192
Prajwol Raj Neupane <prajwolr88@gmail.com>
2023-11-18 18:31:52 +0545

    Revert " changed the title"
    This reverts commit 5ac87446d6535044b12ee60349d76cca644955c7.

--- js/scripts.js
+++ js/scripts.js
@@ -25,20 +25,20 @@ $('.menu-btn').click(function () {
     $('.menu-btn i').toggleClass("active");
 });
 
-// typing text animation script
-var typed = new Typed(".typing", {
-    strings: ["YouTuber", "Developer", "Designer", "Student"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
-var typed = new Typed(".typing-2", {
-    strings: ["YouTuber", "Developer", "Designer", "Student"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Designer", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
 // owl carousel script
 $('.carousel').owlCarousel({
