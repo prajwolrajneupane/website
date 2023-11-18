dc6339c3067ecec6bd73ef81efbc2fe38e4bfadd
Prajwol Raj Neupane <prajwolr88@gmail.com>
2023-11-18 18:30:04 +0545

    reverted

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Designer", "Student"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Developer", "Designer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Designer", "Student"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Designer", "Student"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
