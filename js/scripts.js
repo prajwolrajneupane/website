c692a9deb87dfaa5874c290acac7038016934133
P.raj-Neupane <116416310+prajwolrajneupane@users.noreply.github.com>
2023-11-18 20:03:25 +0545

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -27,14 +27,14 @@ $('.menu-btn').click(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Designer", "Student"],
+        strings: [ "Developer", "Designer", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Designer", "Student"],
+        strings: [ "Developer", "Designer", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
